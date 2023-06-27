49a14fa71f8cc506c1b89df68d5a20e2e0cdb1e9
Katherine Vail <jabberduckygame@gmail.com>
2023-06-26 21:18:11 -0500

    Completed Kanye and Ron conversation API

---
+++ KanyeRonAPI/KanyeVSRon.cs
@@ -0,0 +1,33 @@
+﻿using Newtonsoft.Json.Linq;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KanyeRonAPI
+{
+    public class KanyeVSRon
+    {
+        public static void Kanye()
+        {
+            var client = new HttpClient();
+            var kanyeURL = "https://api.kanye.rest/";
+            var kanyeResponse = client.GetStringAsync(kanyeURL).Result;
+            var kanyeQuote = JObject.Parse(kanyeResponse).GetValue("quote").ToString();
+
+            Console.WriteLine($"then Kanye said to Ron, '{kanyeQuote}'");
+            Console.WriteLine();
+        }
+
+        public static void Ron()
+        {
+            var client = new HttpClient();
+            var ronURL = "https://ron-swanson-quotes.herokuapp.com/v2/quotes";
+            var ronResponse = client.GetStringAsync(ronURL).Result;
+            var ronQuote = JArray.Parse(ronResponse);
+
+            Console.WriteLine($"and then Ron said to Kanye, '{ronQuote[0]}'");
+        }
+    }
+}
---
+++ KanyeRonAPI/Program.cs
@@ -0,0 +1,16 @@
+﻿// See https://aka.ms/new-console-template for more information
+namespace KanyeRonAPI 
+{ 
+public class Program
+{
+    static void Main(string[] args)
+    {
+        Console.Write("Did you hear about the wild conversation between Kayne and Ron? I didn't hear what Ron said first, but ");
+        for (int i = 0; i < 5; i++)
+        {
+                KanyeVSRon.Kanye();
+                KanyeVSRon.Ron();
+        }
+    }
+}
+}
